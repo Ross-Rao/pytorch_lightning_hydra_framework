467fcee934ea656135c3e9fc607065002e9e5b35
Ross-Rao <rj55623371@163.com>
2025-02-14 17:34:42 +0800

    add callbacks instances

--- pytorch_lightning_framework/Trainer/callbacks.py
+++ pytorch_lightning_framework/Trainer/callbacks.py
@@ -1,5 +1,5 @@
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import *
 from pytorch_lightning_framework.DataModule.random_dataset import RandomDataModule
 from pytorch_lightning_framework.Model.simple_model import SimpleModel
 
@@ -7,21 +7,31 @@ from pytorch_lightning_framework.Model.simple_model import SimpleModel
 __all__ = ['get_callbacks']
 
 
-def get_callbacks(callback_cfg: dict):
+def get_callbacks(callback_cfg: dict, extra_callbacks: list = None):
     """
     transform callback config to callback instance, config are expected from yaml files.
 
+    :param extra_callbacks: list of extra callback instance.
     :param callback_cfg: dict of callback config, key must be in cfg_dt.keys()
     :return: list of callback instance
     """
+
     cfg_dt = {
         'ModelCheckpoint': ModelCheckpoint,
+        'EarlyStopping': EarlyStopping,
+        'TQDMProgressBar': TQDMProgressBar,
     }
+    assert set(callback_cfg.keys()).issubset(set(cfg_dt.keys())), \
+        f"callback config key must be in {cfg_dt.keys()}"
     callback_lt = []
-    for key in cfg_dt.keys():
+    for key in callback_cfg.keys():
         callback = cfg_dt[key](**callback_cfg[key])
         callback_lt.append(callback)
-    return callback_lt
+
+    if extra_callbacks is None:
+        return callback_lt
+    else:
+        return callback_lt + extra_callbacks
 
 
 if __name__ == "__main__":
@@ -32,7 +42,9 @@ if __name__ == "__main__":
     # 配置 ModelCheckpoint 回调
     callback_dt = {
         'ModelCheckpoint': dict(filename='model_{epoch:02d}_{val_loss:.2f}', monitor='val_loss',
-                                save_top_k=3, mode='min', save_last=True)
+                                save_top_k=3, mode='min', save_last=True),
+        'EarlyStopping': dict(monitor='val_loss', patience=5, mode='min'),
+        'TQDMProgressBar': dict(refresh_rate=1),
     }
     callbacks = get_callbacks(callback_dt)
 
@@ -40,7 +52,7 @@ if __name__ == "__main__":
     trainer = Trainer(
         max_epochs=100,
         callbacks=callbacks,
-        accelerator="auto",  # 自动选择设备（CPU/GPU）
+        accelerator="auto",
         enable_progress_bar=True
     )
 
