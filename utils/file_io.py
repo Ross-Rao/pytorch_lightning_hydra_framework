3ffe1e2f85a628eddd6e63ddac18c246d3341029
Ross-Rao <rj55623371@163.com>
2025-02-24 10:36:52 +0800

    update file.io loader

--- utils/file_io.py
+++ utils/file_io.py
@@ -1,9 +1,20 @@
+from typing import Literal
 import SimpleITK as sitk
+import numpy as np
 from torchvision.datasets.folder import default_loader
 
 
-def loader(path: str):
+__all__ = ['loader']
+
+
+def loader(path: str, return_type: Literal['obj', 'nparray'] = 'obj'):
     if '.nii.gz' in path:
-        return sitk.ReadImage(path, sitk.sitkFloat32)
+        if return_type == 'nparray':
+            return sitk.GetArrayFromImage(sitk.ReadImage(path, sitk.sitkFloat32))
+        else:
+            return sitk.ReadImage(path, sitk.sitkFloat32)
     else:
-        return default_loader(path)
+        if return_type == 'nparray':
+            return np.array(default_loader(path))
+        else:
+            return default_loader(path)
