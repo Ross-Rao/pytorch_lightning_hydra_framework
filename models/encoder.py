bd7f762e984ee0edf5cb2b7c402feb342fdd1ad9
Ross-Rao <rj55623371@163.com>
2025-03-05 19:20:40 +0800

    add comment

--- models/encoder.py
+++ models/encoder.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 from torchvision.models.resnet import resnet34, resnet50, resnet18
 # local import
 from models.attention import Attention
-from utils.util import get_multi_attr
 
 __all__ = ['ResNetEncoder']
 
@@ -57,9 +56,9 @@ class ResNetEncoder(nn.Module):
 
 def drop_path(x, drop_prob: float = 0., training: bool = False):
     """
-    Obtained from: github.com:rwightman/pytorch-image-models
+    Obtained from: https://github.com/huggingface/pytorch-image-models/blob/main/timm/layers/drop.py
     Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    This is the same as the DropConnect impl I created for EfficientNet, etc. networks, however,
     the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
     See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
     changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
@@ -77,7 +76,7 @@ def drop_path(x, drop_prob: float = 0., training: bool = False):
 
 class DropPath(nn.Module):
     """
-    Obtained from: github.com:rwightman/pytorch-image-models
+    Obtained from: https://github.com/huggingface/pytorch-image-models/blob/main/timm/layers/drop.py
     Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
     """
 
