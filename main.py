136cd0cc06e29c66e7d9c9f932412cd956f4a7fc
Ross-Rao <rj55623371@163.com>
2025-03-16 21:22:13 +0800

    save test result & add seed

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import hydra
+# local import
 import os
 import sys
 import logging
 from datetime import datetime
+# package import
+import hydra
+import pandas as pd
+import pytorch_lightning as pl
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.loggers import TensorBoardLogger
-import pytorch_lightning as pl
 # local import
 from module.example_module import ExampleModule
 from module.monai_data_module import MonaiDataModule
@@ -63,6 +66,10 @@ def main(cfg: DictConfig):
     )
     logger.info("trainer built.")
 
+    # set seed
+    seed = cfg.get("dataset").get("split").get("seed")
+    pl.seed_everything(seed, workers=True)
+
     # build data Module
     dataset_config = cfg.get("dataset")
     datamodule = MonaiDataModule(**dataset_config)
@@ -74,10 +81,16 @@ def main(cfg: DictConfig):
     logger.info("model built.")
 
     # train & test model
-    trainer.fit(model, datamodule)
+    trainer.fit(model, datamodule, ckpt_path=cfg.get("ckpt_path", None))
     logger.info("training finished.")
+
     result = trainer.test(model, datamodule)
     logger.info(f"test result: {result}")
+    pd.DataFrame(result).to_csv(os.path.join(work_dir, 'test_result.csv'))
+
+    result = trainer.test(model, datamodule, ckpt_path="best")
+    logger.info(f"test result: {result}")
+    pd.DataFrame(result).to_csv(os.path.join(work_dir, 'best_result.csv'))
     # trainer.reset_train_dataloader()  # hydra will not reset dataloader automatically
     # trainer.reset_val_dataloader()  # if you use multirun, you need to reset dataloader manually
 
